8f4d43ad0ff6c839f5f8fe978e3057242a98d955
sbigelo <sean.bigelow1@gmail.com>
2021-05-12 16:42:12 -0400

    commit tree fd66b81e65528f77bd88885bb6c427d2531c3784

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+class Polygon {
+   
+    constructor(sides) {
+        this.sides = sides;
+        this.count = this.sides.length;
+    }
+   
+    get countSides() {
+        return this.count
+    }
+
+    get perimeter() {
+        return this.sides.reduce(function(memo, i) {
+            return memo + i
+        })
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+        let triangle = this.sides
+        if (triangle[0] + triangle[1] > triangle[2] && triangle[1] + triangle[2] > triangle[0] && triangle[2] + triangle[0] > triangle[1] && this.count === 3) {
+            return true
+        }
+        return false
+    }
+}
+
+class Square extends Polygon {
+    
+    get isValid() {
+        let square = this.sides
+        if (square[0] === square[1] && square[1] === square[2] && square[2] === square[3]) {
+            return true
+        }
+        return false
+    }
+
+    get area() {
+        let areee = this.sides
+        return areee[0] * areee[1]
+    }
+}
